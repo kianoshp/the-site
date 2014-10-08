f9c9138e952a3e64317c8cbced8c95f022c12449
kianoshp <kianoshp@gmail.com>
2014-10-07 23:44:23 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// app.use(favicon(__dirname + '/app/img/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'app')));
+
+app.get('/', function(req, res) {
+    // res.render('index.jade', 
+    //   {
+    //     title : 'Generic title will go here'
+    //    ,description: 'Describe what you are doing'
+    //    ,author: pjson.author
+    //    ,analyticssiteid: 'XXXXXXX' 
+    //   });
+    fs.readFile(__dirname + '/index.html', 'utf8', function(err, text) {
+        res.send(text);
+    });
+});
+
+// app.get('/new', routes);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('500', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('500', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+// module.exports = app;
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
